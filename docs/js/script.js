2147580895a233755e643051d92a6710ea3a3788
Wayne Eaker <wayne@zengenuity.com>
2023-07-28 14:31:34 -0400

    Move theme to docs folder

---
+++ docs/js/script.js
@@ -0,0 +1,98 @@
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
+// Preloader js
+$(window).on("load", function () {
+  "use strict";
+  $(".preloader").fadeOut(0);
+});
+
+(function ($) {
+  "use strict";
+
+  // tab
+  $(".tab-content")
+    .find(".tab-pane")
+    .each(function (idx, item) {
+      var navTabs = $(this).closest(".code-tabs").find(".nav-tabs"),
+        title = $(this).attr("title");
+      navTabs.append(
+        '<li class="nav-item"><a class="nav-link" href="#">' +
+          title +
+          "</a></li>"
+      );
+    });
+
+  $(".code-tabs ul.nav-tabs").each(function () {
+    $(this).find("li:first").addClass("active");
+  });
+
+  $(".code-tabs .tab-content").each(function () {
+    $(this).find("div:first").addClass("active");
+  });
+
+  // accordion-collapse
+  $(".accordion-collapse").on("show.bs.collapse", function () {
+    $(this).siblings(".accordion-header").addClass("active");
+  });
+  $(".accordion-collapse").on("hide.bs.collapse", function () {
+    $(this).siblings(".accordion-header").removeClass("active");
+  });
+
+  //post slider
+  $(".post-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4500,
+    dots: false,
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="prevArrow"><i class="fas fa-angle-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="nextArrow"><i class="fas fa-angle-right"></i></button>',
+  });
+
+  // videoPopupInit
+  function videoPopupInit() {
+    var $videoSrc;
+    $(".video-play-btn").click(function () {
+      $videoSrc = $(this).data("src");
+    });
+    $(".video-modal").on("show.bs.modal", function (e) {
+      var parents = $(this).parents('.field__item');
+      if (parents.length > 0) {
+        var modal = $(this).detach();
+        parents.first().append(modal);
+      }
+      $(this).find('iframe').each(function () {
+        $(this).width('100%');
+        $(this).height('100%');
+      });
+    });
+    $(".video-modal").on("shown.bs.modal", function (e) {
+      $(this).find('iframe').each(function () {
+        if ($(this).attr('src') == null || $(this).attr('src') == '') {
+          $(this).attr('src', $videoSrc);
+        }
+      });
+    });
+    $(".video-modal").on("hide.bs.modal", function (e) {
+      $(this).find('iframe').each(function () {
+        $(this).attr('src', $(this).attr('src'));
+      });
+    });
+  }
+  videoPopupInit();
+
+  // table of content
+  new ScrollMenu("#TableOfContents a", {
+    duration: 400,
+    activeOffset: 40,
+    scrollOffset: 10,
+  });
+})(jQuery);
